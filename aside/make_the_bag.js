0103fc3b27ff6c63fdbf6e3b77db5f6047421868
David Melsheimer <dave.melshman@gmail.com>
2022-06-09 14:51:43 -0400

    wondering why the dice won't show their faces as the SVGs

--- aside/make_the_bag.js
+++ aside/make_the_bag.js
@@ -1,29 +1,75 @@
-// properties are: die-body-color, die-writing-color, number-of-dots, rune-shape, number-value
+// properties are: 0:die-body-color, 1:die-shape-stroke-color, 2:number-of-dots, 3:rune-shape, 4:number-value
 // white", "black", "one", "circle", "5" 
+
 let svgNS = "http://www.w3.org/2000/svg";
+
 //grab dice container to append to later
-let diceContainer = document.querySelector('dice-container');
+let diceContainer = document.querySelector('.dice-container');
+console.log(diceContainer);
+
 //grab svg defs tag to append face symbols to
 let svgDefsTag = document.getElementById('defs');
 
-for(let [dice, dieIndex] of dice_bag_simple){
-    //build div to contain the eventual <use> containing the face declaration
-    let thisDie = document.createElement("div");
-    thisDie.classList.add("die");
-    if(dice[0][0] == 'red'){thisDie.classList.add('red')};
-    
-    for(let [dieFace, faceIndex] of dice){
-        //build symbol to house each item of the face declaraiton
-        let svgSymbol = document.createElementNS(svgNS, 'symbol');
-        svgSymbol.setAttributeNS(svgNS, 'id', `die${dieIndex}_${faceIndex}`);
+//for each element in dice_bag array (being a 6-element array itself),
+//build <use> tags for each face, and append use tags into a symbol tag with
+//a serialized name
+dice_bag_simple.forEach((die, dieIndex) => {
 
-        console.log(svgSymbol);
-        //build each face
+    //make div to house all 6 faces of current die
+    let thisDieDiv = document.createElement('div');
+    thisDieDiv.setAttribute('class', 'die');
+    thisDieDiv.setAttribute('id', `die_${dieIndex}`);
 
-        // WHEN YOU SIT BACK DOWN, BUILD FACE-GENERATING LOGIC.  SHOULD WE BUILD SYMBOLS AND USE THEM, 
-        // OR PLACE THE SET OF SYMBOLS DIRECTLY INTO THE RENDERED DIVS IN THE BODY?
-        // <symbol id="die1_face1"></symbol>
-        // <symbol id="die1_face2"></symbol>
-    }
+    die.forEach((face, faceIndex) => {
+        let svgFaceSymbol = document.createElementNS(svgNS, 'symbol');
+        let faceSymbolID = `die${dieIndex}_face${faceIndex}`;
+        svgFaceSymbol.setAttributeNS(svgNS, 'id', faceSymbolID);
+
+        //check properties of each face and include required <use> tags in symbol
+        //only the shape need be black/red, dots are always black here
+        if(face[3] != 'none'){
+            let shapeUseTag = document.createElementNS(svgNS, 'use');
+            shapeUseTag.setAttributeNS(svgNS, 'href', `#${face[3]}`);
+            shapeUseTag.setAttributeNS(svgNS, 'stroke', `${face[1]}`)
+            svgFaceSymbol.appendChild(shapeUseTag);
+        }
+        if(face[2] != 'none'){
+            let dotsUseTag = document.createElementNS(svgNS, 'use');
+            dotsUseTag.setAttributeNS(svgNS, 'href', `#dots-${face[2]}`);
+            svgFaceSymbol.appendChild(dotsUseTag);
+        }
+        if(face[4] != ''){
+            let valueUseTag = document.createElementNS(svgNS, 'use');
+            valueUseTag.setAttributeNS(svgNS, 'href', `value-${face[4]}`);
+            svgFaceSymbol.appendChild(valueUseTag);
+        }
+        svgDefsTag.appendChild(svgFaceSymbol);
+        thisDieDiv.appendChild(useFaceAndAppendToDieDiv(faceSymbolID, faceIndex));
+        // console.log(thisDieDiv);
+    });
+    console.log(thisDieDiv);
+    diceContainer.appendChild(thisDieDiv);
+});
+
+
+function useFaceAndAppendToDieDiv(faceSymbolID, faceIndex){
+    //make die face div
+    let faceDiv = document.createElement('div');
+    faceDiv.setAttribute('class', `die-face face-${faceIndex}`);
+
+    //make svg tag
+    let svgTag = document.createElementNS(svgNS, 'svg');
+    svgTag.setAttributeNS(svgNS, 'viewBox', '0 0 200 200');
+
+    //make use tag and set href to faceSymbolID
+    let useTag = document.createElementNS(svgNS, 'use');
+    useTag.setAttributeNS(svgNS, 'href', `#${faceSymbolID}`);
+
+    //make appropriate appendations (real word?)
+    svgTag.appendChild(useTag);
+    faceDiv.appendChild(svgTag);
+    return faceDiv;
 
 }
+//now all the face symbol definitions have been built, so build the die 
+//and append them to the container
